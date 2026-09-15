c499b5f82dc9f97875e16888fdf36834d7f7e112
thanush1229 <thanushthota19@gmail.com>
2026-09-15 13:22:16 +0530

    Add files via upload

---
+++ comparision.operatoers.exercise.py
@@ -0,0 +1,8 @@
+# python comparison operators exercise
+a = 10
+b = 5
+result = a > b # greater than comparsion operators
+print("result of",a, ">",b,"is",result)
+
+result = a < b # less than comparsion operators
+print("result of",a, "<",b,"is",result)
