479d6afdbbab2027bc095da847baf555550526c3
thanush1229 <thanushthota19@gmail.com>
2026-09-17 12:11:52 +0530

    Add files via upload

---
+++ unit1_task-10_relation-operators.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate relational operators
+
+a = 10
+b = 20
+
+print("a =", a)
+print("b =", b)
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not equal to
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater than or equal to
+print("a <= b :", a <= b)   # Less than or equal to
