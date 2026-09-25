63a67607f3059a0e51d7dba9954608ea389e1102
thanush1229 <thanushthota19@gmail.com>
2026-09-25 15:34:48 +0530

    Add files via upload

---
+++ uniy1_task-15_type_conversion.py
@@ -0,0 +1,26 @@
+# Python Program to Demonstrate Type Conversion
+
+# Integer to Float
+a = 10
+b = float(a)
+print("Integer to Float:", b)
+
+# Float to Integer
+x = 15.75
+y = int(x)
+print("Float to Integer:", y)
+
+# Integer to String
+num = 25
+text = str(num)
+print("Integer to String:", text)
+
+# String to Integer
+s = "50"
+n = int(s)
+print("String to Integer:", n)
+
+# String to Float
+p = "12.5"
+q = float(p)
+print("String to Float:", q)
